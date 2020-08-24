39484a1910b4de0d3aa592839e4855a7a5f6a6df
salva <salvaruehling@gmail.com>
2020-08-24 21:10:05 +0200

    learn with leaky relu

--- src/example_applications/MNIST/small_mnist.py
+++ src/example_applications/MNIST/small_mnist.py
@@ -1,5 +1,5 @@
 import numpy as np
-from src.activation_functions import ReLU, Softmax
+from src.activation_functions import ReLU, Softmax, LeakyReLU
 from src.evaluation import plot_loss_and_accuracy, accuracy
 from src.loss_functions import SquaredLoss
 from src.neural_net.layers import InputLayer, Layer
@@ -16,7 +16,7 @@ Xtest, Ytest = np.loadtxt('small_mnist/mnist_small_test_in.txt', delimiter=','),
 (X, Y), (Xval, Yval) = split(X, Y, train_frac=0.9)
 Y = to_categorical(Y)
 
-hidden_layer_act = ReLU()
+hidden_layer_act = LeakyReLU(alpha=0.01)
 layers = [InputLayer(X.shape[1]),
           Layer(25, hidden_layer_act),
           Layer(25, hidden_layer_act),
@@ -24,6 +24,6 @@ layers = [InputLayer(X.shape[1]),
 
 nn = NeuralNetwork(layers)
 nn.compile(SquaredLoss(), metric=accuracy)
-history = nn.fit(X, Y, Xval, Yval, learning_rate=0.01, n_epochs=100, batch_size=32)
+history = nn.fit(X, Y, Xval, Yval, learning_rate=0.003, n_epochs=100, batch_size=16)
 nn.print_predict(Xtest, Ytest)
 plot_loss_and_accuracy(history)
